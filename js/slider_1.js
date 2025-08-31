e3f1821779fab781c4615506a36adac1f1a0ba5b
Zerubbabel CNJ <wokahom@gmail.com>
2025-09-01 00:57:57 +0100

    slider1 commit

---
+++ js/slider_1.js
@@ -0,0 +1,12 @@
+
+  let current = 0;
+  const slides = document.querySelectorAll('.hero-card');
+
+  function showNext() {
+    slides[current].classList.remove('active');
+    current = (current + 1) % slides.length;
+    slides[current].classList.add('active');
+  }
+
+  setInterval(showNext, 1500); // every 1 sec
+
